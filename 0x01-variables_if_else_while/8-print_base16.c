e5a3bafb093caccef689eac90df89588c46e5a2c
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-16 15:42:45 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print base 16 in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	char a;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
