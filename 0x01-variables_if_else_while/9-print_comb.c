6ebc852b919047db2e465f116b0be10cb51781ef
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-16 16:24:25 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 11; n < 21; n++)
+	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
-		if (n != 20)
+		putchar(n + '0');
+		if (n < 9)
 		{
 			putchar(',');
 			putchar(' ');
