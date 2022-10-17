8e6303386bd9f3ed4459f1fbadc92cdbabb4aa9c
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-17 04:26:26 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,13 @@
 int main(void)
 {
 	long long int lli;
+	long int li;
+	unsigned long ul;
 
-	printf("size of a char: %lu.\n", (unsigned long)sizeof(char));
-	printf("size of an int: %lu.\n", (unsigned long)sizeof(int));
-	printf("size of a long int: %lu.\n", (unsigned long)sizeof(long int));
-	printf("size of a lli: %lu.\n", (unsigned long)sizeof(lli));
-	printf("size of a float: %lu.\n", (unsigned long)sizeof(float));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("size of a li: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a lli: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
