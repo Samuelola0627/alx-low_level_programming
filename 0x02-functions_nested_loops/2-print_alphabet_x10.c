f856db039013dfa7bd16afeb00cd2eedec54b3d7
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-18 09:46:27 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,11 @@ void print_alphabet_x10(void)
 	int n = 1;
 	char az;
 
-	while (n++ <= 10)
+	while (n <= 10)
 	{
+		n++
 		for (az = 'a'; az <= 'z'; az++)
 			_putchar(az);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
