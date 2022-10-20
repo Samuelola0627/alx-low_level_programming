c15d70f17179ed65aaa4d6c8544ead5b0936827c
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-20 14:51:26 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,13 @@ void more_numbers(void)
 	{
 		i++
 		for (c = '0'; c <= 14; c++)
-			_putchar(c);
+		{
+			if (c >= 10)
+				_putchar(1);
+			_putchar(c % 10 + '0');
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
+
+
