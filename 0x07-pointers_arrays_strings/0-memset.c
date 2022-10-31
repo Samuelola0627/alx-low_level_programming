1c1264aa8f3ca4f1b9811a34464cee70efbefd1e
Aro Kola Samuel <samuelola0627@gmail.com>
2022-10-31 13:08:07 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- *
+ * _memset - fills memory with constant byte
+ * @s: a string
+ * @b: a character
+ * @n: an interger
+ * Return: a string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
