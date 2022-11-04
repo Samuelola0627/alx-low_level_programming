58b79062e1ea44c1e2386618178b1933276b6fba
Aro Kola Samuel <samuelola0627@gmail.com>
2022-11-04 14:50:41 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, n1, n2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	n1 = atoi argv[1];
+	n2 = atoi argv[2];
+	result = n1 * n2;
+	printf("%d\n", result);
+}
