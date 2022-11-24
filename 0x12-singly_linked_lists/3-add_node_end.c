de15e4d35d6d048be814d6f91e6c88cef098be14
Aro Kola Samuel <samuelola0627@gmail.com>
2022-11-24 07:10:09 -0800

    Task 0 - Task 4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,425 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * add_node_end - function that adds a new node at the end of list.
+ * @head: pointer to singly linked list.
+ * @str: pointer to signly linked list.
+ *
+ * str needs to be duplicated.
+ * You are allowed to use strdup.
+ *
+ * Return: The address of the new element or NULL if it failed.
+ *
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
-	size_t nchar;
+	list_t *new_node, *last;
+	size_t length = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(list_t));
+	/* if it fails returb NULL */
+	if (new_node == NULL)
 		return (NULL);
-
-	new->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	new->len = nchar;
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
+	/* loop through the string to find length */
+	while (str[length])
+		length++;
+	/* access the length of new_node and assign it to length */
+	new_node->len = length;
+	/* access the list of new_node and duplicate it */
+	new_node->str = strdup(str);
+	/* if there is no head/linked list make new_node as head */
+	if (*head == NULL)
 	{
-		*head = new;
+		new_node->next = *head; /*this step isn't needed really */
+		*head = new_node;
 	}
 	else
-	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
+	{	/**
+		 * the new node is going to be the last node so make next,
+		 * of it as NULL
+		 */
+		new_node->next = NULL;
+		last = *head;
+		/* traverse till last node */
+		while (last->next)
+			last = last->next;
+		/* change the next of last node */
+		last->next = new_node;
 	}
+	return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
 
-	return (*head);
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
+}#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *  * add_node_end - function that adds a new node at the end of list.
+ *   * @head: pointer to singly linked list.
+ *    * @str: pointer to signly linked list.
+ *     *
+ *      * str needs to be duplicated.
+ *       * You are allowed to use strdup.
+ *        *
+ *         * Return: The address of the new element or NULL if it failed.
+ *          *
+ *           */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+		list_t *new_node, *last;
+			size_t length = 0;
+
+				new_node = malloc(sizeof(list_t));
+					/* if it fails returb NULL */
+					if (new_node == NULL)
+								return (NULL);
+						/* loop through the string to find length */
+						while (str[length])
+									length++;
+							/* access the length of new_node and assign it to length */
+							new_node->len = length;
+								/* access the list of new_node and duplicate it */
+								new_node->str = strdup(str);
+									/* if there is no head/linked list make new_node as head */
+									if (*head == NULL)
+									{
+												new_node->next = *head; /*this step isn't needed really */
+														*head = new_node;
+									}
+									else
+									{	/**
+										  		 * the new node is going to be the last node so make next,
+												 * 		 * of it as NULL
+												 * 		 		 */
+												new_node->next = NULL;
+														last = *head;
+																/* traverse till last node */
+																while (last->next)
+																				last = last->next;
+																		/* change the next of last node */
+																		last->next = new_node;
+									}
+										return (new_node);
 }
